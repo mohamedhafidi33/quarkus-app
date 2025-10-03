4045676396b6b671bd513ad556fcae6a8f7fa411
Mohamed Hafidi <mohamed.hafidi.mh@gmail.com>
2025-10-03 16:11:45 +0200

    FizzBuzzExecutor

--- src/main/java/org/tirana/lio/quarkus/QuarkusApp.java
+++ src/main/java/org/tirana/lio/quarkus/QuarkusApp.java
@@ -1,23 +1,29 @@
 package org.tirana.lio.quarkus;
 
+import org.tirana.lio.quarkus.util.FizzBuzzExecutor;
 import org.tirana.lio.quarkus.util.GreetingUtil;
 
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.inject.Inject;
 
 @QuarkusMain
-public class QuarkusApp implements QuarkusApplication{
+public class QuarkusApp implements QuarkusApplication {
 
 	GreetingUtil greetingUtil;
-	
-	public QuarkusApp(GreetingUtil greetingUtil) {
+
+	FizzBuzzExecutor fizzBuzzExecutor;
+
+	public QuarkusApp(GreetingUtil greetingUtil, FizzBuzzExecutor fizzBuzzExecutor) {
 		super();
 		this.greetingUtil = greetingUtil;
+		this.fizzBuzzExecutor = fizzBuzzExecutor;
 	}
 
 	@Override
 	public int run(String... args) throws Exception {
 		System.out.println(this.greetingUtil.getGreeting());
+		fizzBuzzExecutor.execute();
 		return 0;
 	}
 
